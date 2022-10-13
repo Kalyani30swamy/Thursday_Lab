ffbc2e8956a7155b0bf0c75d5fd1151bdc1714d7
Kalyani30swamy <113984331+Kalyani30swamy@users.noreply.github.com>
2022-10-13 17:47:46 +0530

    Add files via upload

---
+++ Lab3.13.10.2022/Q1/Palindrome_call.java
@@ -0,0 +1,13 @@
+package Inheritance;
+
+public class Palindrome_call extends Q1_palindrome 	//Extend the class Q1_palindrome with the main class Palindrome_call
+{
+
+	public static void main(String[] args) 			//Main method
+	{
+		// TODO Auto-generated method stub
+		palidrome();								//call the method from Q1_palindrome class
+	}
+	
+
+}
